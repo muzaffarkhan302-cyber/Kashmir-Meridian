8db4b65e58ec028bc03357b0a876af37c6257af9
muzaffarkhan302-cyber <muzaffarkhan302@gmail.com>
2026-04-28 03:52:31 +0530

    Update import paths to include .tsx extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router'
-import Home from './pages/Home'
-import Builder from './pages/Builder'
-import Itinerary from './pages/Itinerary'
+import Home from './pages/Home.tsx'
+import Builder from './pages/Builder.tsx'
+import Itinerary from './pages/Itinerary.tsx'
 
 export default function App() {
   return (
